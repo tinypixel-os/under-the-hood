bf102f6c6a075c34ed81936818aa5db859b53bfa
tinypixel <him23code@gmail.com>
2025-02-09 15:44:58 +0530

    chore: add TokenType enum

--- lexer.ts
+++ lexer.ts
@@ -2,3 +2,18 @@ let x = 45;
 
 // [ LetToken, IdentifierToken, EqualsToken, NumberToken, SemicolonToken];
 
+export const TokenType {
+    LetToken,
+    IdentifierToken,
+    EqualsToken,
+    NumberToken,
+    SemicolonToken,
+
+    OpenParenToken,
+    CloseParenToken,
+
+    AddToken,
+    MinusToken,
+    MultiplyToken,
+    DivideToken,
+}
